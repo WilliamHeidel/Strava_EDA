1231b70a667f2cfa98a6ad869c9bb467d6378727
WilliamPHeidel <wpheidel@energetics.com>
2025-05-30 06:42:05 -0400

    Added more DBT columns and run prod build after test.

--- strava_data_dbt/models/silver/silver_strava_activities.sql
+++ strava_data_dbt/models/silver/silver_strava_activities.sql
@@ -1,3 +1,8 @@
 
 SELECT *
+    ,[distance] / 1609.344 AS distance_miles
+    ,CAST([moving_time] AS float) / 60 AS moving_time_minutes
+    ,CAST([moving_time] AS float) / 3600 AS moving_time_hours
+    ,CAST([elapsed_time] AS float) / 60 AS elapsed_time_minutes
+    ,CAST([elapsed_time] AS float) / 3600 AS elapsed_time_hours
 FROM {{ ref('bronze_strava_activities') }}
