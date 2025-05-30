70f787187ba0e8d8feb76599c576076c9cb30f29
WilliamPHeidel <wpheidel@energetics.com>
2025-05-30 07:02:22 -0400

    Added date columns.

--- strava_data_dbt/models/silver/silver_strava_activities.sql
+++ strava_data_dbt/models/silver/silver_strava_activities.sql
@@ -5,4 +5,6 @@ SELECT *
     ,CAST([moving_time] AS float) / 3600 AS moving_time_hours
     ,CAST([elapsed_time] AS float) / 60 AS elapsed_time_minutes
     ,CAST([elapsed_time] AS float) / 3600 AS elapsed_time_hours
+    ,CAST([start_date] AS date) AS start_date_datevalue
+    ,CAST([start_date_local] AS date) AS start_date_local_datevalue
 FROM {{ ref('bronze_strava_activities') }}
